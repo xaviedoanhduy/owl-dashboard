6d7fa05088bfd09360116ac8b4fd497052a16230
xavie <moncoeur2k1@gmail.com>
2024-06-17 23:10:51 +0700

    add kpi card compoment

--- static/src/components/sale/sale_dashboard.js
+++ static/src/components/sale/sale_dashboard.js
@@ -4,82 +4,99 @@ import { registry } from "@web/core/registry";
 import { KpiCard } from "./kpi_card/kpi_card";
 import { ChartRenderer } from "./chart_renderer/chart_renderer";
 import { useService } from "@web/core/utils/hooks";
-
+import { formatMonetary } from "@web/views/fields/formatters";
 const { Component, onWillStart, useRef, onMounted, useState } = owl;
 const { DateTime } = luxon;
 
 export class OwlSaleDashboard extends Component {
     setup() {
+        this.resModel = "sale.order";
+
         this.state = useState({
-            quotations: {
-                value: 10,
-                percentage: 30,
-            },
-            period: "all",
-            currentDate: DateTime.now(),
-            prevDate: null
+            period: 30,
         });
 
+        this.currentCompany = useService("company").currentCompany
         this.orm = useService("orm");
-
         onWillStart(async () => {
-            await this.onchagePeriod();
+            await this.onChangePeriod();
         });
     }
 
-    async onchagePeriod() {
+    async onChangePeriod() {
         this.getDate();
         await this.getQuotationsCount();
+        await this.getOrders();
     }
 
     getDate() {
-        switch (this.state.period) {
-            case "today":
-                this.state.prevDate = this.state.currentDate.startOf("day");
-                break;
-            case "sevenDays":
-                this.state.prevDate = this.state.currentDate.minus({ days: 7 });
-                break;
-            case "thisMonth":
-                this.state.prevDate = this.state.currentDate.startOf("month");
-                break;
-            case "thisYear":
-                this.state.prevDate = this.state.currentDate.startOf("year");
-                break;
-            case "threeMonth":
-                this.state.prevDate = this.state.currentDate.minus({ months: 3 });
-                break;
-            default:
-                this.state.prevDate = null;
-                return;
-        }
+        this.state.currentDate = DateTime.now().minus({ days: this.state.period }).toISODate()
+        this.state.prevDate = DateTime.now().minus({ days: (this.state.period * 2) }).toISODate()
     }
 
     async getQuotationsCount() {
         const currentDomain = [["state", "in", ["draft", "sent"]]];
-        if (this.state.period !== "all" && this.state.prevDate) {
-            currentDomain.push(
-                ["date_order", ">=", this.state.prevDate.toISODate()],
-                ["date_order", "<", this.state.currentDate.toISODate()]
-            );
-        }
-        const saleOrderCurrentCount = await this.orm.searchCount("sale.order", currentDomain);
-        this.state.quotations.value = saleOrderCurrentCount;
+        if (this.state.period > 0) 
+            currentDomain.push(["date_order", ">", this.state.currentDate]);
         
+        const saleOrderCurrentCount = await this.orm.searchCount(this.resModel, currentDomain);
+        
+        let prevDomain = [["state", "in", ["draft", "sent"]]];
         let saleOrderPrevCount = 0;
-        if (this.state.period !== "all" && this.state.prevDate) {
-            const prevDomain = [
-                ["state", "in", ["draft", "sent"]],
-                ["date_order", "<", this.state.prevDate.toISODate()]
-            ];
-            saleOrderPrevCount = await this.orm.searchCount("sale.order", prevDomain);
+        if (this.state.period > 0) {
+            prevDomain.push(
+                ["date_order", ">", this.state.prevDate],
+                ["date_order", "<=", this.state.currentDate]
+            )
+            saleOrderPrevCount = await this.orm.searchCount(this.resModel, prevDomain);
         }
+        const percentage = ((saleOrderCurrentCount - saleOrderPrevCount) / saleOrderPrevCount) * 100;
+        this.state.quotations = {
+            value: saleOrderCurrentCount,
+            percentage: percentage.toFixed(2)
+        }
+    }
 
-        if (saleOrderPrevCount > 0) {
-            const percentage = ((saleOrderCurrentCount - saleOrderPrevCount) / saleOrderPrevCount) * 100;
-            this.state.quotations.percentage = percentage;
-        } else {
-            this.state.quotations.percentage = 0;
+    async getOrders() {
+        const currentDomain = [["state", "in", ["sale", "done"]]];
+        
+        if (this.state.period > 0) 
+            currentDomain.push(["date_order", ">", this.state.currentDate]);
+        const saleOrderCurrentCount = await this.orm.searchCount(this.resModel, currentDomain);
+        
+        let prevDomain = [["state", "in", ["sale", "done"]]];
+        let saleOrderPrevCount = 0;
+        if (this.state.period > 0) {
+            prevDomain.push(
+                ["date_order", ">", this.state.prevDate],
+                ["date_order", "<=", this.state.currentDate]
+            )
+            saleOrderPrevCount = await this.orm.searchCount(this.resModel, prevDomain);
+        }
+        const percentage = ((saleOrderCurrentCount - saleOrderPrevCount) / saleOrderPrevCount) * 100;
+
+        const currentRevenue = await this.orm.readGroup(
+            this.resModel, 
+            currentDomain,
+            ["amount_total:sum"],
+            []
+        )
+        const prevRevenue = await this.orm.readGroup(
+            this.resModel, 
+            prevDomain,
+            ["amount_total:sum"],
+            []
+        )
+        const revenuePercentage = (
+            (currentRevenue[0]["amount_total"] - prevRevenue[0]["amount_total"])
+            / prevRevenue[0]["amount_total"]
+        ) * 100
+        
+        this.state.orders = {
+            value: saleOrderCurrentCount,
+            percentage: percentage.toFixed(2),
+            revenue: `$${formatMonetary(currentRevenue[0]["amount_total"] / 1_000)}K`,
+            revenuePercentage: revenuePercentage.toFixed(2),
         }
     }
 }
